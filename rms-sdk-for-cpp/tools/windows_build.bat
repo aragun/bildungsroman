8087346a0b3f81efc3ff6c163aec692b51a7b3e1
Anurag Pratik <anuprat@microsoft.com>
2016-11-15 18:22:14 -0800

    bat changes

--- rms-sdk-for-cpp/tools/windows_build.bat
+++ rms-sdk-for-cpp/tools/windows_build.bat
@@ -1,9 +1,9 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools\VsDevCmd.bat"
+REM call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools\VsDevCmd.bat"
 cd ..\sdk
-call qmake sdk.pro -spec win32-msvc2013 "CONFIG+=debug" "CONFIG+=qml_debug"
+call C:\Qt\5.7\msvc2013\bin\qmake.exe sdk.pro -spec win32-msvc2013 "CONFIG+=debug" "CONFIG+=qml_debug"
 call nmake
 cd ..\samples
-call qmake sdk.pro -spec win32-msvc2013 "CONFIG+=debug" "CONFIG+=qml_debug"
+call C:\Qt\5.7\msvc2013\bin\qmake.exe samples.pro -spec win32-msvc2013 "CONFIG+=debug" "CONFIG+=qml_debug"
 call nmake
 cd ..\bin
 copy tests\*UnitTest* .
