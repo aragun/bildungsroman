564a3747d4529561d557768f9065df3c5179e1ab
Anurag Pratik <anuprat@microsoft.com>
2016-11-15 18:36:49 -0800

    bat_delta

--- rms-sdk-for-cpp/tools/windows_build.bat
+++ rms-sdk-for-cpp/tools/windows_build.bat
@@ -1,10 +1,9 @@
-REM call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools\VsDevCmd.bat"
 cd ..\sdk
 call C:\Qt\5.7\msvc2013\bin\qmake.exe sdk.pro -spec win32-msvc2013 "CONFIG+=debug" "CONFIG+=qml_debug"
-call nmake
+call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin\nmake.exe"
 cd ..\samples
 call C:\Qt\5.7\msvc2013\bin\qmake.exe samples.pro -spec win32-msvc2013 "CONFIG+=debug" "CONFIG+=qml_debug"
-call nmake
+call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin\nmake.exe"
 cd ..\bin
 copy tests\*UnitTest* .
 rmscryptoUnitTestsd.exe -xunitxml > tests\rmsUnitTestResults.txt
