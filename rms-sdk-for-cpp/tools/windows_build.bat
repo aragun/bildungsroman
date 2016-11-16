d340419882f85e89d5cccefdfe0b543eeba006b7
Anurag Pratik <anuprat@microsoft.com>
2016-11-16 10:24:41 -0800

    batdel1

--- rms-sdk-for-cpp/tools/windows_build.bat
+++ rms-sdk-for-cpp/tools/windows_build.bat
@@ -1,3 +1,5 @@
+mkdir ..\third_party
+xcopy C:\third_party ..\third_party /e
 cd ..\sdk
 call C:\Qt\5.7\msvc2013\bin\qmake.exe sdk.pro -spec win32-msvc2013 "CONFIG+=debug" "CONFIG+=qml_debug"
 call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin\nmake.exe"
